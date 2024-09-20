e0c1b826d55f3b5794298ac72d7af5da26bd0dc1
mishaKuschenko <liahimkusenko@gmail.com>
2024-09-20 23:44:04 +0300

    Update urls.go

--- internal/urls.go
+++ internal/urls.go
@@ -1,14 +1,1 @@
-package internal
 
-import (
-	"fmt"
-	"net/http"
-)
-
-func Home_page(page http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(page, "Let's GO!")
-}
-
-func About_page(page http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(page, "about page")
-}
